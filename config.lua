7130c283a38073d2b6494d5d0978cd0ba57d94f1
Robert <robert.strietzel@gmail.com>
2015-08-01 17:56:48 +0200

    fix

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ wakeup_pin = 8
 gpio.mode(8, gpio.OUTPUT)
 gpio.write(wakeup_pin, gpio.HIGH)
 
-uart.setup( 0, 115200, 8, 0, 1, 0 )
+uart.setup( 0, 19200, 8, 0, 1, 0 )
 -- start
 uart.write( 0, 128)
 tmr.delay(50000)
