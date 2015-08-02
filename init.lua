26961da3daf953eaf6f0d4781ae339d0ea1302cc
Arian Sanusi <arian@sanusi.de>
2015-08-02 21:46:52 +0200

    removed lua-code not used in this branch

--- init.lua
+++ init.lua
@@ -1,10 +1,1 @@
-dofile('config.lua')
-
-gpio.mode(0,gpio.OUTPUT)
-gpio.write(0,gpio.LOW) -- low is the new high, i.e. LED on
-
-wifi.setmode(wifi.STATION)
-wifi.sta.config(ESSID,PSK)
-tmr.delay(500000)
-dofile('server.lua')
-dofile('uart_rx_timeout.lua')
+udp2uart.lua
